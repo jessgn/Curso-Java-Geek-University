26381b56c4df57cb676b546d1107522faf47e72b
Jéssica Nascimento <59929856+jessgn@users.noreply.github.com>
2020-02-16 16:56:59 -0300

    Update SomaTriplo.java

--- Exercícios - Lista1/SomaTriplo.java
+++ Exercícios - Lista1/SomaTriplo.java
@@ -1,5 +1,20 @@
 package exerciciosCurso;
+import java.util.Scanner;
+
+ 	// Leia um numero inteiro e imprima a soma do sucessor de seu triplo com o antecessor de seu dobro
 
 public class SomaTriplo {
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("Digite um numero: ");
+		int n,ant,suc,soma;
+		n = teclado.nextInt();
+		
+		suc=(n*n*n)+1;
+		ant=(n*n)-1;
+		soma = suc+ant;
+		System.out.println("O numero digitado foi: " + n + ". O sucessor de seu triplo é: "+suc+". O antecessor de seu dobro é: "+ant+ ". A soma dos dois é: " + soma);
+		
+	}
 
 }
